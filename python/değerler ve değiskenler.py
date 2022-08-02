da66a73c15de8d9103b38ef2ddac5b9a876eacef
severarif <109618902+severarif@users.noreply.github.com>
2022-08-02 17:33:31 +0300

    food recipe first

---
+++ python/değerler ve değiskenler.py
@@ -0,0 +1,139 @@
+sayı=10
+print(sayı)
+x=10
+print(x)
+x=20
+print(x)
+x=20+30
+print(x)
+a="5"+str(9)
+print(a)
+a=int("5")+120
+print(a)
+x=10
+print("x="+str(x))
+print("bu yemeğe puanım",str(x),"üzerinden",str(x))
+print("bu yemeğe puanım \n ",str(x),"üzerinden \n",str(x))
+y=20
+print("y="+str(y))
+print("y=\n"+str(y))
+
+
+
+x,  y, z =100, -45, 0
+#x=100            # konulan yer sonuçta görülmez
+#y=-45          #   , print baska sey + aynı şey için
+#z=0
+print("x=",x,"\n y=",y," \n z=",z)
+
+a=10
+print("a'nın  ilk değeri" , a, "ve tipi",type(a))
+
+b="ARİF SEVER"
+print("b'nin ilk değeri" , b ,"ve tipi",type(b))
+#del değiskenleri silmek için kullanılır
+
+
+
+x=123.53
+print( type( x))
+print(int(x))
+d =85
+print(float(d))
+
+
+
+
+
+
+
+
+
+sayı=78.95
+print(sayı)
+sayı2=sayı
+print(round(sayı))
+sayı3=79.3
+print(round(sayı3))
+
+x=93.34836
+print(round(x,0))
+print(round(x,1))
+print(round(x,2))
+print(round(x,3))
+print(round(x,4))
+print(round(x,5))
+print(type(round(x)))
+print(type(round(x,1)))
+
+
+x=12345.98765
+print(round(x,0))
+print(round(x,-1))
+print(round(x,-2))
+print(round(x,-3))
+print(round(x,-4))
+print(round(x,-5))
+
+a=5
+
+#del(değer1int(input("bir sayı giriniz")))
+#del(değer2int(input("diğer sayıyı giriniz")))
+#del(toplam = değer1+değer2)
+#del(print(değer1, "+",değer2,"=", toplam))
+
+sayı1= 40
+sayı2=6
+kelime1="arif"
+kelime2="sever"
+
+toplama1=sayı1+sayı2
+print(toplama1)
+toplama2=kelime1+kelime2
+print(toplama2)
+fark1=sayı1-sayı2
+print(fark1)
+çarpma=sayı1*sayı2
+print(çarpma)
+çarpma2=sayı2*kelime1
+print(çarpma2)
+çarpma3=kelime2*sayı2
+print(çarpma3)
+bölme=sayı1/sayı2
+print(bölme)
+print(8/4)
+kere_bulma=sayı1//sayı2
+print(kere_bulma)
+kalan_bulma=sayı1%sayı2
+print(kalan_bulma)
+
+
+kuvvet_alma=sayı1**sayı2
+print(kuvvet_alma)
+print(2**5)
+
+
+
+
+
+x=4
+y=10.2
+toplam=x+y
+print(toplam)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
